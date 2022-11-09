ec52a7fef8cb89360cefc348def50295ffece012
matteo <matteo.faginali@gmail.com>
2022-11-09 19:04:26 +0100

    LoginUtenti

--- LoginUtenti/script.js
+++ LoginUtenti/script.js
@@ -1,38 +1,76 @@
-const utenti= ["ciccioGamer", "icardi", "khaby", "rocco", "anna", "matteo"];
-const password= ["ciccio1", "wandanara", "tiktok", "siffredi33" , "tatangelo", "faginali"];
+const utenti = ["ciccioGamer", "icardi", "khaby", "rocco", "anna"];
+const password = ["ciccio1", "wandanara", "tiktok", "siffredi33", "tatangelo"];
+let newUser = document.getElementById("newUser");
+let newPass = document.getElementById("newPass");
 
+function login() {
+  let user = document.getElementById("user").value;
+  let pass = document.getElementById("pass").value;
 
-function login(){
-    let temp;
-    let utente = document.getElementById("username").value;
-    let passworda = document.getElementById("password").value;
-
-    for(let i=0 ;i<utenti.length; i++){
-        if(utenti[i]==utente){
-            temp = i;
-        }
+  for (let i = 0; i < utenti.length; i++) {
+    if (user == utenti[i] && pass == password[i]) {
+      successo.innerHTML = "Benvenuto " + user;
+      successo.classList.remove("error");
+      successo.classList.add("success");
+      return;
     }
-
-    if(temp == null){
-        document.getElementById("messaggio").innerHTML = "il nome utente o la passwod non sono corretti"
-        document.getElementById("messaggio").classList.toggle("errore");
-    }
-    else{
-        if(utenti[temp]==utente && passworda==password[temp]){
-            document.getElementById("messaggio").innerHTML = "login effettuato con successo"
-            document.getElementById("messaggio").classList.toggle("giusto")
-        }
-    }
+  }
+  successo.innerHTML = "Username o password errati";
+  successo.classList.add("error");
+  successo.classList.remove("success");
 }
 
-function hide(){
-    document.getElementById("si").classList.toggle("hide")
+function apparire() {
+  document.getElementById("prova").classList.remove("hidden");
 }
-hide();
 
-function registra(){
-    utenti[utenti.length] = document.getElementById("name").value
-    password[password.length] = document.getElementById("pass").value
-    console.log("successo")
-    hide();
+function createAccount() {
+  let user = document.getElementById("newUser").value;
+  let pass = document.getElementById("newPass").value;
+
+  for (let i = 0; i < utenti.length; i++) {
+    if (user == utenti[i]) {
+      alert("Username già esistente");
+      newUser.value = "";
+      newPass.value = "";
+      return;
+    }
+  }
+  utenti[utenti.length] = user;
+  password[password.length] = pass;
+  console.log(utenti);
+  console.log(password);
+
+  document.getElementById("prova").classList.add("hidden");
+}
+
+let prec = 0;
+let prec2 = 0;
+let test = document.getElementById("test");
+let successo = document.getElementById("successo/no");
+
+function sposta() {
+  const a = [-50, 0, 50];
+  const b = [0, 100, 150, 200];
+  if (
+    document.getElementById("pass").value == "" &&
+    document.getElementById("user").value == ""
+  ) {
+    //random form 0 to 10
+
+    do {
+      var random = Math.floor(Math.random() * 3);
+    } while (random == prec);
+    do {
+      var random2 = Math.floor(Math.random() * 4);
+    } while (random2 == prec2);
+    prec = random;
+    prec2 = random2;
+    console.log(random);
+    console.log(random2);
+    test.style.marginLeft = a[random] + "%";
+    test.style.marginTop = b[random2] + "px";
+    successo.innerHTML = "Non hai inserito username e password";
+    successo.classList.add("error");
+  }
 }
